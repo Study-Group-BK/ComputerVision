0d16f8aea48ac45df78608867b1e7ca1849910bb
Tran Tieu Binh <tieubinhco@gmail.com>
2020-12-26 11:17:32 +0700

    Console Test C#

--- ConsoleTestOpenCV/ConsoleApp1/Program.cs
+++ ConsoleTestOpenCV/ConsoleApp1/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
 
 namespace ConsoleApp1
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+           Mat M = new Mat(3, 3,DepthType.Cv8U, 1);
+            M.SetTo(new MCvScalar(1 / 9));
+                Console.WriteLine(M.GetDataPointer());
+            Console.ReadLine();
         }
     }
 }
